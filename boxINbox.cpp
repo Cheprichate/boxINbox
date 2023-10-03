2c69bfbfb592a432dc55def4608bb9623a411267
Cheprichate <petrovich15071998@mail.ru>
2023-10-03 21:59:25 +0400

    Добавьте файлы проекта.

---
+++ boxINbox.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+int a;
+int b;
+int c;
+
+int M;
+int N;
+int K;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Размеры коробки которую нужно вложить. \n";
+    std::cin >> a >> b >> c;
+    std::cout << "Размеры коробки которую нужно вложить. \n";
+    std::cin >> M >> N >> K;
+    if(a <= M && b <= N && c <= K || a <= N && b <= M && c <= K){
+        std::cout << "Получиться вложить. \n";
+    }
+    else if (c <= a && a <= c && K <= b || K <= c && N <= b && K <= a) {
+        std::cout << "Получиться вложить. \n";
+    }
+    else {
+        std::cout << "Вложить не получиться.";
+
+    }
+}
